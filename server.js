ebb36ea8847d494be7936e5073e4f39c8d7262bc
Martinus Tampubolon <martinus.tampubolon@xtremax.com>
2021-08-12 18:41:52 +0700

    update app

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+// Build for Xendit Work Trial by Martinus Tampubolon
+ 
 
 var express = require('express');
 var os = require('os');
 var app = express();
-
 const used = process.memoryUsage().heapUsed / 1024 / 1024;
+var candidate_name = "Martinus Tampubolon";
+
+//This variables used for version:2
+let ts = Date.now();
+let date_ob = new Date(ts);
+// current Date
+let date = date_ob.getDate();
+// current month
+let month = date_ob.getMonth() + 1;
+// current year
+let year = date_ob.getFullYear();
+var current_date = [year + "-" + month + "-" + date];
+var candidate_name = "Martinus Tampubolon";
+
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -13,17 +27,28 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-//console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
 
+//uncomment this section for apps version 1
+/*app.get('/api', function (req, res) {
+      res.send(Hello Xendit! From docker:'+'{OS:'+os.platform()+'}{Architecture:'+os.arch()+'}{Version:'+os.release()+'}{ContainerID:'+os.hostname()+'}'); 
 
+});
+*/
+
+//uncomment this section for apps version 2
 app.get('/api', function (req, res) {
-    //res.send('Hello Xendit from Martin, '+'This host OS:'+os.platform()+' '+'Architecture:'+os.arch()+' Version:'+os.release()+' Memory:'+os.totalmem()+'CPU:'+os.hostname());
-	res.send(`The script uses approximately ${Math.round(used * 100) / 100} MB`)
+        res.send(`Hello Xendit! From docker: {OS:${os.platform()}}{OSArchitecture:${os.arch()}}{OSVersion:${os.release()}}{ContainerID:${os.hostname()}}.
+        This is a simple app by ${candidate_name} for work trial start from 2021-8-12 to ${current_date}`);
 });
 
-//app.get('/api', function (req, res) {
-//    res.send('Memory:'+os.totalmem()+' '+'CPU:'+os.cpus());
-//});
+
+//uncomment this section for apps version 3
+/*
+app.get('/api', function (req, res) {
+	res.send(`Hello Xendit! From docker: {OS:${os.platform()}}{OSArchitecture:${os.arch()}}{OSVersion:${os.release()}}{ContainerID:${os.hostname()}``}.                                    
+	//This is a simple app from ${candidate_name} for work trial start from 2021-8-12 to ${current_date}`);	
+});
+*/
 
 var port = process.env.PORT || 80;
 var server = app.listen(port, function () {
@@ -39,9 +64,11 @@ process.on("SIGTERM", () => {
     server.close();
 });
 
-/*const used = process.memoryUsage().heapUsed / 1024 / 1024;
-app.get('/api', function (req, res) {
-    //res.send('Hello Xendit from Martin, '+'This host OS:'+os.platform()+' '+'Architecture:'+os.arch()+' Version:'+os.release()+' Memory:'+os.totalmem()+'CPU:'+os.hostname());
-	res.send(`The script uses approximately ${Math.round(used * 100) / 100} MB`)
-});
-*/
+
+
+//console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+//app.get('/api', function (req, res) {
+//    res.send('Memory:'+os.totalmem()+' '+'CPU:'+os.cpus());
+//res.send(`The script uses approximately ${Math.round(used * 100) / 100} MB`)
+//});
+
